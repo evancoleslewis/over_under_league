2a68096c9098e62e0d56facdf14853e047b16d24
evancoleslewis <evancoles14@gmail.com>
2023-11-13 17:15:18 -0800

    Add variables for projections

--- config.py
+++ config.py
@@ -1,4 +1,17 @@
 STANDINGS_URL = 'https://www.basketball-reference.com/leagues/NBA_2024_standings.html'
+PROJECTIONS_URL = 'https://www.basketball-reference.com/friv/playoff_prob.html'
 
 WESTERN_CONFERENCE_ID = "div_confs_standings_W"
 EASTERN_CONFERENCE_ID = "div_confs_standings_E"
+
+WESTERN_PROJECTIONS_ID = "all_projected_standings_w"
+EASTERN_PROJECTIONS_ID = "all_projected_standings_e"
+
+ID_DICT = {
+    'standings' : {
+        'western'  : "div_confs_standings_W"
+        ,'eastern' : "div_confs_standings_E"}
+    ,'projections' : {
+        'western'  : "all_projected_standings_w"
+        ,'eastern' : "all_projected_standings_e"}
+}
